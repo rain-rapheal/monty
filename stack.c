c0d518b4128fda42826477b8f154beec8e526597
rain-rapheal <rain.phy567@gmail.com>
2023-12-15 01:40:39 -0800

    update

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - print top
+ * @head: stack_head
+ * @counter: line_number
+ * Return: no_return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
